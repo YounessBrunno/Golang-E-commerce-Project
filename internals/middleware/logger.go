92c52ff7d5df60f8980ad9d8e60cb3353605e5e5
youness oukhadjou <brunnotools@gmail.com>
2026-06-08 11:32:35 +0100

    added logger middleware to enhance logging functionality

--- internals/middleware/logger.go
+++ internals/middleware/logger.go
@@ -1,2 +1,24 @@
-package middleware
+package main
 
+import (
+	"log"
+	"net/http"
+	"time"
+)
+
+func loggingMiddleware(next http.Handler) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		start := time.Now()
+
+		log.Printf(
+			"%s %s %s\n",
+			r.Method,
+			r.URL.Path,
+			start.Format(time.RFC3339),
+		)
+
+		next.ServeHTTP(w, r)
+	})
+}
