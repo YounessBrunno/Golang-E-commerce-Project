7914de42d130091d2f2b8ae9d74ddd78125b9a75
youness oukhadjou <brunnotools@gmail.com>
2026-06-09 13:26:22 +0100

    working on proper dep injection

--- internals/products/handler.go
+++ internals/products/handler.go
@@ -4,3 +4,9 @@ package products
 type Handler struct {
    productService ProductService
 }
+
+func NewHandler(productService ProductService) *Handler {
+   return &Handler{
+	 productService: productService,
+   }
+}
